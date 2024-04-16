6eb3a875a3f68623d5cdeb6439a2ba0ee234e7a3
Maitane <maitane@gmail.com>
2024-04-16 21:39:29 +0200

    Tercera fase proyecto (Clase y Principal creadas y comentadas)

--- src/directorioTrabajoGrandeMaitane/Clase.java
+++ src/directorioTrabajoGrandeMaitane/Clase.java
@@ -1,5 +1,9 @@
 package directorioTrabajoGrandeMaitane;
 
 public class Clase {
+	/**
+	 * Esta clase Clase tendrá un constructor por defecto que muestra por pantalla 
+	 * "Soy un objeto de la clase y acabo de ser creado"
+	 */
 
 }
